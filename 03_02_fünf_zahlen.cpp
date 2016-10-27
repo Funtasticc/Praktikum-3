473c7bbf8392e418bc292bd08d43545d79ac4538
Steffen <steffen.moeller1993@gmx.net>
2016-10-27 09:00:24 +0200

    Update 03_02_fünf_zahlen.cpp

--- 03_02_fünf_zahlen.cpp
+++ 03_02_fünf_zahlen.cpp
@@ -3,75 +3,84 @@ using namespace std;
 
 int main()
 {
-	int zahl1, zahl2, zahl3, zahl4, zahl5, min, max, pos_min, pos_max;
-
+	int zahl1, zahl2, zahl3, zahl4, zahl5, min, max, pos_min, pos_max = 0;
+	// Zahl 1 ist erstmal min, max, pos_min und pos_max
 	cout << "Bitte geben Sie die 1. Zahl ein: ";
 	cin >> zahl1;
 	min = zahl1;
 	max = zahl1;
 	pos_min = 1;
 	pos_max = 1;
-///////////////////////////////////////////////////////////////////////
-		cout << "Bitte geben Sie die 2. Zahl ein: ";
-		cin >> zahl2;
-	
-		if (zahl2 < zahl1)
-			{
-			min = zahl2;
-			pos_min = 2;
-			}
-		
-		if (zahl2 > zahl2)
-			{		
-			zahl2 = max;
-			pos_max = 2;
-			}
-//////////////////////////////////////////////////////////////////////////////////
-		cout << "Bitte geben Sie die 3. Zahl ein: ";
-		cin >> zahl3;
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "Bitte geben Sie die 2. Zahl ein: ";
+	cin >> zahl2;
+	// trifft eine Bedingung der If's zu, wird die Variable min und pos_min neu gesetzt, trifft 
+	// sie nicht zu, bleibt min = zahl1 und pos_min = 1 
+	if (zahl2 < zahl1)
+	{
+		min = zahl2;
+		pos_min = 2;
+	}
+	// trifft eine Bedingung der If's zu, wird die Variable max und pos_max neu gesetzt, trifft 
+	// sie nicht zu, bleibt max = zahl1 und pos_max = 1 
+	if (zahl2 > zahl1)
+	{
+		zahl2 = max;
+		pos_max = 2;
+	}
+	if (zahl1 == zahl2)
+	{
+		zahl1 = max;
+		zahl2 = min;
+		pos_min = 1;
+		pos_max = 1;
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "Bitte geben Sie die 3. Zahl ein: ";
+	cin >> zahl3;
 
-		if (zahl3 < zahl2 && zahl3 < zahl1)
-			{	
-				min = zahl3;
-				pos_min = 3;
-			}
-	
-		if (zahl3 > zahl2 && zahl3 < zahl1)
-			{
-				max = zahl3;
-				pos_max = 3;
-			}
-///////////////////////////////////////////////////////////////////////////////
-		cout << "Bitte geben Sie die 4. Zahl ein: ";
-		cin >> zahl4;
-	
-		if (zahl4 < zahl3 && zahl4 < zahl2 && zahl4 < zahl1)
-			{
-			zahl4 = min;
-			pos_min = 4;
-			}
+	if (zahl3 < min)
+	{
+		min = zahl3;
+		pos_min = 3;
+	}
 
-		if (zahl4 > zahl3 && zahl4 > zahl2 && zahl4 > zahl1)
-			{
-			zahl4 = max;
-			pos_max = 4;
-			}
-/////////////////////////////////////////////////////////////////////////////////////	
-		cout << "Bitte geben Sie die 5. Zahl ein: ";
-		cin >> zahl5;
-		
-		if (zahl5 < zahl4 && zahl5 < zahl3 && zahl5 < zahl2 && zahl5 < zahl1)
-			{
-				min = zahl5;
-				pos_min = 5;
-			}
+	if (zahl3 > max)
+	{
+		max = zahl3;
+		pos_max = 3;
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "Bitte geben Sie die 4. Zahl ein: ";
+	cin >> zahl4;
 
-		if (zahl5 > zahl4 && zahl5 > zahl3 && zahl5 > zahl2 && zahl5 > zahl1)
-				{
-				max = zahl5;
-				pos_max = 5;
-				}
-////////////////////////////////////////////////////////////////////////////////
+	if (zahl4 < min)
+	{
+		zahl4 = min;
+		pos_min = 4;
+	}
+
+	if (zahl4 > max)
+	{
+		zahl4 = max;
+		pos_max = 4;
+	}
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "Bitte geben Sie die 5. Zahl ein: ";
+	cin >> zahl5;
+
+	if (zahl5 < min)
+	{
+		min = zahl5;
+		pos_min = 5;
+	}
+
+	if (zahl5 > max)
+	{
+		max = zahl5;
+		pos_max = 5;
+	}
+	///////////////////////////////////////////////////////////////////////////////////////////////////
 	cout << "Die " << pos_min << ". Zahl war die kleinste der eingegebenen Zahlen und lautet: " << min << endl;
 	cout << "Die " << pos_max << ". Zahl war die groesste der eingegebenen Zahlen und lautet: " << max << endl;
 	system("PAUSE");
